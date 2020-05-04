51aa0ca7d6ec2a02a1f2a35529019abb8e3724d9
Xunin <victorniubo>
2020-05-04 18:47:43 -0300

    layout morse

---
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/Frases.java
@@ -0,0 +1,79 @@
+package br.pro.hashi.ensino.desagil.projeto1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.util.ArrayList;
+
+import android.os.Bundle;
+
+public class Frases extends AppCompatActivity {
+
+    private int contador = -1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frases);
+
+        ArrayList<String> listaFrases = new ArrayList<String>();
+
+
+
+        TextView textExample = findViewById(R.id.text_example);
+        TextView textExample2 = findViewById(R.id.text_example2);
+        TextView textExample3 = findViewById(R.id.text_example3);
+        TextView textExample4 = findViewById(R.id.text_example4);
+        TextView textExample5 = findViewById(R.id.text_example5);
+        TextView textExample6 = findViewById(R.id.text_example6);
+        TextView textExample7 = findViewById(R.id.text_example7);
+        TextView textExample8 = findViewById(R.id.text_example8);
+        TextView textExample9 = findViewById(R.id.text_example9);
+        TextView textExample10 = findViewById(R.id.text_example10);
+
+//        EditText editExample = findViewById(R.id.edit_example);
+        Button botao1 = findViewById(R.id.button_example1);
+        Button botao2 = findViewById(R.id.button_example2);
+
+        listaFrases.add(textExample3.getText().toString());
+        listaFrases.add(textExample4.getText().toString());
+        listaFrases.add(textExample5.getText().toString());
+        listaFrases.add(textExample6.getText().toString());
+        listaFrases.add(textExample7.getText().toString());
+        listaFrases.add(textExample8.getText().toString());
+        listaFrases.add(textExample9.getText().toString());
+        listaFrases.add(textExample10.getText().toString());
+
+
+
+
+        botao1.setOnClickListener((view) -> {
+            this.contador -= 1;
+            if(contador <  0){
+                contador = 7;
+            }
+            String content = listaFrases.get(this.contador);
+            textExample.setText("Ferrão: " + content);
+
+        });
+
+        botao2.setOnClickListener((view) -> {
+            this.contador += 1;
+            if(this.contador==8) {
+                this.contador = 0;
+            }
+            String content = listaFrases.get(this.contador);
+            textExample.setText("Ferrão: " + content);
+
+
+        });
+
+
+
+
+
+    }
+}
--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MainActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MainActivity.java
@@ -2,75 +2,46 @@ package br.pro.hashi.ensino.desagil.projeto1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
 
-    private int contador = -1;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        ArrayList<String> listaFrases = new ArrayList<String>();
-
-
-
-        TextView textExample = findViewById(R.id.text_example);
-        TextView textExample2 = findViewById(R.id.text_example2);
-        TextView textExample3 = findViewById(R.id.text_example3);
-        TextView textExample4 = findViewById(R.id.text_example4);
-        TextView textExample5 = findViewById(R.id.text_example5);
-        TextView textExample6 = findViewById(R.id.text_example6);
-        TextView textExample7 = findViewById(R.id.text_example7);
-        TextView textExample8 = findViewById(R.id.text_example8);
-        TextView textExample9 = findViewById(R.id.text_example9);
-        TextView textExample10 = findViewById(R.id.text_example10);
-
-//        EditText editExample = findViewById(R.id.edit_example);
-        Button botao1 = findViewById(R.id.button_example1);
-        Button botao2 = findViewById(R.id.button_example2);
-
-        listaFrases.add(textExample3.getText().toString());
-        listaFrases.add(textExample4.getText().toString());
-        listaFrases.add(textExample5.getText().toString());
-        listaFrases.add(textExample6.getText().toString());
-        listaFrases.add(textExample7.getText().toString());
-        listaFrases.add(textExample8.getText().toString());
-        listaFrases.add(textExample9.getText().toString());
-        listaFrases.add(textExample10.getText().toString());
-
-
-
-
-        botao1.setOnClickListener((view) -> {
-            this.contador -= 1;
-            if(contador <  0){
-                contador = 7;
+        Button botaoFrases = findViewById(R.id.button3);
+        botaoFrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent frases = new Intent(MainActivity.this, Frases.class);
+                startActivity(frases);
             }
-            String content = listaFrases.get(this.contador);
-            textExample.setText("Ferrão: " + content);
+        });
+
+        TextView mensagem = findViewById(R.id.textView5);
+
+        Button botaoMorse = findViewById(R.id.button2);
+
+        botaoMorse.setOnClickListener((view) -> {
+            mensagem.setText(mensagem.getText().toString()+".");
 
         });
 
-        botao2.setOnClickListener((view) -> {
-            this.contador += 1;
-            if(this.contador==8) {
-                this.contador = 0;
-            }
-            String content = listaFrases.get(this.contador);
-            textExample.setText("Ferrão: " + content);
+        botaoMorse.setOnLongClickListener((view) -> {
+            mensagem.setText(mensagem.getText().toString()+"-");
 
-
+            return true;
         });
-
-
 
 
     }

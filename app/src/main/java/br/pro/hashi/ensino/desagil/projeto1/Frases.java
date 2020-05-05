835918e8f8ffece0bb39598030591879d88c0ecd
Xunin <victorniubo>
2020-05-05 19:05:25 -0300

    sprint 1 arrumada

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/Frases.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/Frases.java
@@ -2,6 +2,7 @@ package br.pro.hashi.ensino.desagil.projeto1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,28 +25,30 @@ public class Frases extends AppCompatActivity {
 
 
         TextView textExample = findViewById(R.id.text_example);
+        TextView textExample0 = findViewById(R.id.text_example0);
+        TextView textExample1 = findViewById(R.id.text_example1);
         TextView textExample2 = findViewById(R.id.text_example2);
         TextView textExample3 = findViewById(R.id.text_example3);
         TextView textExample4 = findViewById(R.id.text_example4);
         TextView textExample5 = findViewById(R.id.text_example5);
         TextView textExample6 = findViewById(R.id.text_example6);
         TextView textExample7 = findViewById(R.id.text_example7);
-        TextView textExample8 = findViewById(R.id.text_example8);
-        TextView textExample9 = findViewById(R.id.text_example9);
-        TextView textExample10 = findViewById(R.id.text_example10);
+
 
 //        EditText editExample = findViewById(R.id.edit_example);
         Button botao1 = findViewById(R.id.button_example1);
         Button botao2 = findViewById(R.id.button_example2);
 
+        listaFrases.add(textExample0.getText().toString());
+        listaFrases.add(textExample1.getText().toString());
+        listaFrases.add(textExample2.getText().toString());
         listaFrases.add(textExample3.getText().toString());
         listaFrases.add(textExample4.getText().toString());
         listaFrases.add(textExample5.getText().toString());
         listaFrases.add(textExample6.getText().toString());
         listaFrases.add(textExample7.getText().toString());
-        listaFrases.add(textExample8.getText().toString());
-        listaFrases.add(textExample9.getText().toString());
-        listaFrases.add(textExample10.getText().toString());
+
+
 
 
 
@@ -58,6 +61,55 @@ public class Frases extends AppCompatActivity {
             String content = listaFrases.get(this.contador);
             textExample.setText("Ferrão: " + content);
 
+            if(contador == 0){
+                textExample0.setTextColor(Color.RED);
+            }
+            else{
+                textExample0.setTextColor(Color.GRAY);
+            }
+            if(contador == 1){
+                textExample1.setTextColor(Color.RED);
+            }
+            else{
+                textExample1.setTextColor(Color.GRAY);
+            }
+            if(contador == 2){
+                textExample2.setTextColor(Color.RED);
+            }
+            else{
+                textExample2.setTextColor(Color.GRAY);
+            }
+            if(contador == 3){
+                textExample3.setTextColor(Color.RED);
+            }
+            else{
+                textExample3.setTextColor(Color.GRAY);
+            }
+            if(contador == 4){
+                textExample4.setTextColor(Color.RED);
+            }
+            else{
+                textExample4.setTextColor(Color.GRAY);
+            }
+            if(contador == 5){
+                textExample5.setTextColor(Color.RED);
+            }
+            else{
+                textExample5.setTextColor(Color.GRAY);
+            }
+            if(contador == 6){
+                textExample6.setTextColor(Color.RED);
+            }
+            else{
+                textExample6.setTextColor(Color.GRAY);
+            }
+            if(contador == 7){
+                textExample7.setTextColor(Color.RED);
+            }
+            else{
+                textExample7.setTextColor(Color.GRAY);
+            }
+
         });
 
         botao2.setOnClickListener((view) -> {
@@ -68,8 +120,57 @@ public class Frases extends AppCompatActivity {
             String content = listaFrases.get(this.contador);
             textExample.setText("Ferrão: " + content);
 
+            if(contador == 0){
+                textExample0.setTextColor(Color.RED);
+            }
+            else{
+                textExample0.setTextColor(Color.GRAY);
+            }
+            if(contador == 1){
+                textExample1.setTextColor(Color.RED);
+            }
+            else{
+                textExample1.setTextColor(Color.GRAY);
+            }
+            if(contador == 2){
+                textExample2.setTextColor(Color.RED);
+            }
+            else{
+                textExample2.setTextColor(Color.GRAY);
+            }
+            if(contador == 3){
+                textExample3.setTextColor(Color.RED);
+            }
+            else{
+                textExample3.setTextColor(Color.GRAY);
+            }
+            if(contador == 4){
+                textExample4.setTextColor(Color.RED);
+            }
+            else{
+                textExample4.setTextColor(Color.GRAY);
+            }
+            if(contador == 5){
+                textExample5.setTextColor(Color.RED);
+            }
+            else{
+                textExample5.setTextColor(Color.GRAY);
+            }
+            if(contador == 6){
+                textExample6.setTextColor(Color.RED);
+            }
+            else{
+                textExample6.setTextColor(Color.GRAY);
+            }
+            if(contador == 7){
+                textExample7.setTextColor(Color.RED);
+            }
+            else{
+                textExample7.setTextColor(Color.GRAY);
+            }
 
         });
+
 
 
 
